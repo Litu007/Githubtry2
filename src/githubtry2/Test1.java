b2360214680295b661553ed62c50ddeab79b03cf
dibya <dibya@LAPTOP-TQ0BM40A>
2023-08-21 20:32:52 +0530

    first commit....

---
+++ src/githubtry2/Test1.java
@@ -0,0 +1,26 @@
+package githubtry2;
+
+public class Test1 {
+	
+	
+
+	
+		public static void main(String[] args) {
+			
+		
+		String hungary = "yes";
+		if(hungary == "yes")
+			System.out.println("eat something1....");
+		else {
+			
+			System.out.println("do work...........");
+		
+		
+
+			
+	}
+
+	}
+
+
+}
