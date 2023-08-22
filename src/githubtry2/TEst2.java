bab185da80468510d83ea3ff6c3a586d79796eb1
dibya <dibya@LAPTOP-TQ0BM40A>
2023-08-22 11:55:15 +0530

    commit test2

---
+++ src/githubtry2/TEst2.java
@@ -0,0 +1,29 @@
+package githubtry2;
+
+public class TEst2 {
+		
+		
+
+		
+			public static void main(String[] args) {
+				
+			
+			String hungary = "yes";
+			if(hungary == "yes")
+				System.out.println("eat something1....");
+			else {
+				
+				System.out.println("do work...........");
+			
+			
+
+				
+		
+
+		}
+
+
+	}
+
+
+}
